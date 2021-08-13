66ceb8d0b02facefdfa83cedc9ee8da4d56d80f9
snoopy109498 <snoopy109498@gmail.com>
2021-08-13 22:00:00 +0900

    login logout

---
+++ Instagram/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Instagram
+//
+//  Created by dslog sys on 2021/08/11.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
